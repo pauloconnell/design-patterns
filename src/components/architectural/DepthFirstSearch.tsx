4fa8c2821ed92c049a7fa4080b98c17bff482807
Paul O'Connell <p_ollie@hotmail.com>
2025-10-28 15:42:58 -0700

    Update: each section should have hover effect

--- src/components/architectural/DepthFirstSearch.tsx
+++ src/components/architectural/DepthFirstSearch.tsx
@@ -1,4 +1,5 @@
-import React, { useState, CSSProperties } from 'react';
+import React, { useState } from 'react';
+import type { CSSProperties } from 'react';
 
 // Define the type for the props
 type DepthFirstSearchProps = { className?: string; style?: CSSProperties };
@@ -21,13 +22,13 @@ const DepthFirstSearch: React.FC<DepthFirstSearchProps> = ({ className, style })
    };
 
    return (
-      <div className={className} style={style}>
-       
+      <section className={className} style={style}>
          <h1>Depth First Search</h1>
          <h2>DFS</h2>
          Depth First means to drill down as deep as possible, then backtrack
          <br />
-         <button className='btn btn-primary'
+         <button
+            className="btn btn-primary"
             onClick={(e) => {
                handleClick();
                console.log('clicked', e.target);
@@ -39,51 +40,48 @@ const DepthFirstSearch: React.FC<DepthFirstSearchProps> = ({ className, style })
          {/* {showThis.toString()} // If you wanted to see the boolean as text */}
          {showThis ? (
             <div id="dfs">
-               
-
-                  <h3>Depth First Search:</h3>
-                  Associated with using a Stack...but we will use recursion instead
-                  <h4>Used for Binary Tree traversals</h4>
-               
-              <hr />
+               <h3>Depth First Search:</h3>
+               Associated with using a Stack...but we will use recursion instead
+               <h4>Used for Binary Tree traversals</h4>
+               <hr />
                <div className="my-3">
                   <h5 className="bold">
                      In-order -
                      <br />
                   </h5>
-                   <b className="explaination">If performed on a Binary Search Tree (BST), the result is the node values retrieved in ascending (sorted) order.</b>
+                  <b className="explaination">
+                     If performed on a Binary Search Tree (BST), the result is the node
+                     values retrieved in ascending (sorted) order.
+                  </b>
                   1.traverse left
                   <br />
                   2.Visit node <br />
                   3.traverse right <br />
-                 
-                  Using recursion:
                   <br />
                   <code className="text-start d-block">
                      {`let preOrder=function(node) {`} <br />
                      if node==null then return
                      <div className="ms-2">
-                        preOrder(node.left) //recursion
-                        
-                        visit(node)-ie answer.push(node.val) <br />
+                        preOrder(node.left) //recursion visit(node)-ie
+                        answer.push(node.val) <br />
                         preOrder(node.right) // magic
                      </div>
-                     
                   </code>
-                    time complexity O(n)
+                  time complexity O(n)
                </div>
-<hr />
- <div className="my-3">
+               <hr />
+               <div className="my-3">
                   <h5 className="bold">
                      Pre-order
                      <br />
                   </h5>
-                  <b className="explaination">Used to copy existing tree - get root/parents then children</b>
+                  <b className="explaination">
+                     Used to copy existing tree - get root/parents then children
+                  </b>
                   1.Visit node <br />
                   2.traverse left
                   <br />
                   3.traverse right <br />
-                  
                   <br />
                   <code className="text-start d-block">
                      {`let preOrder=function(node) {`} <br />
@@ -95,9 +93,7 @@ const DepthFirstSearch: React.FC<DepthFirstSearchProps> = ({ className, style })
                         <br />
                         preOrder(node.right) // magic
                      </div>
-             
                   </code>
-             
                   time complexity O(n)
                </div>
                <hr />
@@ -106,11 +102,13 @@ const DepthFirstSearch: React.FC<DepthFirstSearchProps> = ({ className, style })
                      Post-order
                      <br />
                   </h5>
-                   <b className="explaination"> Used to delete a tree - must delete children before parents</b>
+                  <b className="explaination">
+                     {' '}
+                     Used to delete a tree - must delete children before parents
+                  </b>
                   1.traverse left <br />
                   2.traverse right <br />
                   3.Visit node <br />
-              
                   <br />
                   <code className="text-start d-block">
                      {`let postOrder=function(node) {`}
@@ -125,17 +123,14 @@ const DepthFirstSearch: React.FC<DepthFirstSearchProps> = ({ className, style })
                         visit(node) //-ie answer.push(node.val) <br />
                      </div>
                   </code>
-                  
                   time complexity O(n)
-                  
-                  
                </div>
                <br />
             </div>
          ) : (
             <div className="my-2">`Click button to show details`</div>
          )}
-      </div>
+      </section>
    );
 };
 

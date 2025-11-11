2dd09cfae5483b3ec164e4dbd216bba3e857a8f3
Paul O'Connell <p_ollie@hotmail.com>
2025-11-10 17:04:30 -0800

    Update: Must null check root

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import './styles/custom-bootstrap.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
